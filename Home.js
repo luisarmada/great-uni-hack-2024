c95320bbbefebf63353d9bb6b3619b4296517bc2
Mohamad Ediab <mohdediab@gmail.com>
2024-11-10 00:14:23 +0000

    Real template

--- Home.js
+++ Home.js
@@ -1,5 +1,15 @@
-// JavaScript to toggle between light and meme themes
-function toggleTheme() {
-    document.body.classList.toggle('meme-theme');
+// Animate each letter of modal buttons for a bouncy Mario effect
+for (const text of document.querySelectorAll(".modal-action-text")) {
+    const letters = text.textContent.split("");
+    text.innerHTML = "";  
+  
+    letters.forEach((letter, index) => {
+      const span = document.createElement("span");
+      span.className = "modal-action-text-letter";
+      span.style.animationDelay = `${index * 200}ms`;
+      span.style.animationDuration = `${(letters.length * 300) + 1000}ms`;
+      span.innerHTML = letter;
+      text.appendChild(span);
+    });
   }
   
